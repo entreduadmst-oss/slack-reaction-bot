b46ba19eddcf1f4bec9ed6407c078451bf7366f8
entreduadmst-oss <entredu.admst@gmail.com>
2026-04-07 00:18:20 +0900

    Update slack-events.js

--- Slack_reaction_bot/api/slack-events.js
+++ Slack_reaction_bot/api/slack-events.js
@@ -1,63 +1,38 @@
 module.exports = async function handler(req, res) {
+  console.log('RUNNING VERSION 7');
+
   try {
-    if (req.method !== 'POST') {
-      return res.status(200).send('ok');
+    if (req.method === 'GET') {
+      return res.status(200).send('VERSION 7 OK');
     }
 
-    const body = req.body;
+    let body = req.body;
+
+    if (typeof body === 'string') {
+      try {
+        body = JSON.parse(body);
+      } catch (e) {
+        console.log('BODY STRING PARSE FAILED');
+      }
+    }
+
+    console.log('METHOD:', req.method);
     console.log('BODY:', JSON.stringify(body));
 
-    if (body.type === 'url_verification') {
-      console.log('url_verification');
+    if (body && body.type === 'url_verification') {
+      console.log('URL VERIFICATION');
       return res.status(200).json({ challenge: body.challenge });
     }
 
-    if (body.type !== 'event_callback') {
-      console.log('skip: not event_callback');
+    if (body && body.type === 'event_callback') {
+      console.log('EVENT CALLBACK RECEIVED');
+      console.log('EVENT:', JSON.stringify(body.event));
       return res.status(200).send('ok');
     }
-
-    const event = body.event;
-    console.log('EVENT TYPE:', event?.type);
-    console.log('REACTION:', event?.reaction);
-
-    if (event?.type !== 'reaction_added') {
-      console.log('skip: not reaction_added');
-      return res.status(200).send('ok');
-    }
-
-    if (!['flag-us', 'us'].includes(event.reaction)) {
-      console.log('skip: not us reaction');
-      return res.status(200).send('ok');
-    }
-
-    const channel = event.item?.channel;
-    const ts = event.item?.ts;
-    const slackBotToken = process.env.SLACK_BOT_TOKEN;
-
-    console.log('CHANNEL:', channel);
-    console.log('TS:', ts);
-    console.log('HAS TOKEN:', !!slackBotToken);
-
-    const postRes = await fetch('https://slack.com/api/chat.postMessage', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${slackBotToken}`
-      },
-      body: JSON.stringify({
-        channel,
-        thread_ts: ts,
-        text: 'test reply from bot'
-      })
-    });
-
-    const postData = await postRes.json();
-    console.log('POST MESSAGE:', JSON.stringify(postData));
 
     return res.status(200).send('ok');
   } catch (error) {
     console.error('ERROR:', error);
-    return res.status(200).send('ok');
+    return res.status(200).send('error');
   }
 };
