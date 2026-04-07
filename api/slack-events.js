cc278f455f11e8caaf87e2fb3fd692613e04f0c0
entreduadmst-oss <entredu.admst@gmail.com>
2026-04-07 12:06:08 +0900

    Update slack-events.js

--- api/slack-events.js
+++ api/slack-events.js
@@ -1,9 +1,26 @@
 module.exports = async function handler(req, res) {
-  console.log('RUNNING VERSION 1');
+  console.log('RUNNING VERSION 2');
 
-  if (req.method === 'GET') {
-    return res.status(200).send('OK');
+  try {
+    if (req.method !== 'POST') {
+      return res.status(200).send('ok');
+    }
+
+    const body = req.body;
+    console.log('BODY:', JSON.stringify(body));
+
+    if (body.type === 'url_verification') {
+      return res.status(200).json({ challenge: body.challenge });
+    }
+
+    if (body.type === 'event_callback') {
+      console.log('EVENT CALLBACK RECEIVED');
+      return res.status(200).send('ok');
+    }
+
+    return res.status(200).send('ok');
+  } catch (error) {
+    console.error('ERROR:', error);
+    return res.status(200).send('error');
   }
-
-  return res.status(200).send('POST OK');
 };
