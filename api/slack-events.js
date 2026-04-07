52832271b64629b52ddc06c9cc16f29f0b82540a
entreduadmst-oss <entredu.admst@gmail.com>
2026-04-07 12:08:45 +0900

    Update slack-events.js

--- api/slack-events.js
+++ api/slack-events.js
@@ -1,22 +1,67 @@
 module.exports = async function handler(req, res) {
-  console.log('RUNNING VERSION 2');
+  console.log('RUNNING VERSION 3');
 
   try {
     if (req.method !== 'POST') {
       return res.status(200).send('ok');
     }
 
-    const body = req.body;
+    let body = req.body;
+
+    if (typeof body === 'string') {
+      try {
+        body = JSON.parse(body);
+      } catch (e) {
+        console.log('BODY STRING PARSE FAILED');
+      }
+    }
+
     console.log('BODY:', JSON.stringify(body));
 
     if (body.type === 'url_verification') {
       return res.status(200).json({ challenge: body.challenge });
     }
 
-    if (body.type === 'event_callback') {
-      console.log('EVENT CALLBACK RECEIVED');
+    if (body.type !== 'event_callback') {
       return res.status(200).send('ok');
     }
+
+    const event = body.event;
+    console.log('EVENT:', JSON.stringify(event));
+
+    if (!event || event.type !== 'reaction_added') {
+      console.log('skip: not reaction_added');
+      return res.status(200).send('ok');
+    }
+
+    if (!['flag-us', 'us'].includes(event.reaction)) {
+      console.log('skip: not US reaction');
+      return res.status(200).send('ok');
+    }
+
+    const channel = event.item?.channel;
+    const ts = event.item?.ts;
+    const slackBotToken = process.env.SLACK_BOT_TOKEN;
+
+    console.log('CHANNEL:', channel);
+    console.log('TS:', ts);
+    console.log('HAS TOKEN:', !!slackBotToken);
+
+    const postRes = await fetch('https://slack.com/api/chat.postMessage', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${slackBotToken}`
+      },
+      body: JSON.stringify({
+        channel,
+        thread_ts: ts,
+        text: 'test reply from bot'
+      })
+    });
+
+    const postData = await postRes.json();
+    console.log('POST MESSAGE:', JSON.stringify(postData));
 
     return res.status(200).send('ok');
   } catch (error) {
